349e3ffd290905f4849a04cda683ec5a59d6b1f4
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2025-10-16 07:38:31 +0900

    version: 1.0.3 に

--- src/manifest.config.ts
+++ src/manifest.config.ts
@@ -2,7 +2,7 @@ import { defineManifest } from '@crxjs/vite-plugin';
 
 export default defineManifest({
   manifest_version: 3,
-  version: '1.0.2',
+  version: '1.0.3',
   default_locale: 'ja',
   name: '__MSG_extensionName__',
   description: '__MSG_extensionDescription__',
